642bb8fb3d6a02c78fe7af723441d1bf11e8a44b
uniqcle <uniqcle@yandex.ru>
2021-11-19 15:00:48 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './styles.css'
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
+
+addBtn.addEventListener('click', function () {
+    state++;
+    render();
+})
+
+subBtn.addEventListener('click', function () {
+    state--;
+    render();
+})
+
+
+asyncBtn.addEventListener('click', function () {
+    setTimeout(() => {
+        state++;
+        render();
+    }, 2000)
+})
+
+
+themeBtn.addEventListener('click', function () {
+    document.body.classList.toggle('dark')
+})
+
+
+render();
+
+
